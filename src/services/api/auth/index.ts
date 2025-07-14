2d7ca52788ce42be4cc75b7562b6482acfaa40a4
quocbaoevizi <quocbaovu15101996@gmail.com>
2025-07-14 22:43:58 +0700

    refactor: code

---
+++ src/services/api/auth/index.ts
@@ -0,0 +1,13 @@
+import api, { ApiResponse } from "..";
+import { ENDPOINTS, LoginResponse } from "../config";
+
+export const loginByDevice = async ({ platform, deviceId }: { platform: string, deviceId: string }) => {
+  const payload = {
+    platform,
+    deviceId,
+  };
+  return await api.post<LoginResponse>(
+    ENDPOINTS.AUTH.LOGIN_BY_DEVICE,
+    payload
+  ) as unknown as ApiResponse<LoginResponse>;
+}
